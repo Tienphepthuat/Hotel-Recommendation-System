5db5eaede2a6fb8e540d819628bf2f2bc47689b4
unknown <Tienphepthuat>
2024-08-26 08:24:00 +0700

    Sửa lỗi đọc file trong GUI1.py, đổi 'error_bad_lines' thành 'on_bad_lines'

--- GUI1.py
+++ GUI1.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1af2a570d3f02fe2cb901a2256badd4be2b3ec026ee3731df65fbe3cce52f29a
-size 16177
+oid sha256:26820ed0df94fe8844b12a547cb456d7dcfe8118f1de26fd4637defe752ddc7f
+size 16184
